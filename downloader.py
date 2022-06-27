684a93a595e3ed0aceba928c2c3d8166d0eba335
Qin Li <liblaf@outlook.com>
2022-06-27 16:31:34 +0800

    *

---
+++ downloader.py
@@ -0,0 +1,209 @@
+import os
+from tqdm import tqdm
+
+
+from thu_learn_lib import LearnHelper
+from thu_learn_lib import ty as types
+from thu_learn_lib.utils import slugify
+
+
+class Downloader(LearnHelper):
+    prefix: str = ""
+    file_size_limit: int = None
+    sync_docs: bool = True
+    sync_work: bool = True
+    sync_submit: bool = True
+
+    def __init__(
+        self,
+        prefix: str = "",
+        file_size_limit: int = None,
+        sync_docs: bool = True,
+        sync_work: bool = True,
+        sync_submit: bool = True,
+    ) -> None:
+        super().__init__()
+        if prefix:
+            self.prefix = prefix
+        else:
+            self.prefix = os.path.join(os.getcwd(), slugify("learn"))
+        self.file_size_limit = file_size_limit
+        self.sync_docs = sync_docs
+        self.sync_work = sync_work
+        self.sync_submit = sync_submit
+
+    def Download(self, url: str, prefix: str, filename: str) -> bool:
+        os.makedirs(prefix, exist_ok=True)
+        filename = slugify(filename)
+        response = self.get(url=url, stream=True)
+        file_size = int(response.headers.get("content-length", 0))
+        if self.file_size_limit:
+            if file_size > self.file_size_limit * 1024:
+                return False
+        chunk_size = 8192  # 8KB
+        with tqdm(
+            desc=filename,
+            total=file_size,
+            unit="B",
+            ascii=True,
+            unit_scale=True,
+            dynamic_ncols=True,
+        ) as bar:
+            with open(os.path.join(prefix, filename), "wb") as file:
+                for chunck in response.iter_content(chunk_size):
+                    file.write(chunck)
+                    bar.update(len(chunck))
+        return True
+
+    def SyncSemester(
+        self, semester_id: str, course_type: types.CourseType = types.CourseType.STUDENT
+    ) -> bool:
+        print(f"Syncing Semester {semester_id} ......")
+        course_list = self.get_course_list(
+            semester_id=semester_id, course_type=course_type
+        )
+        for course in course_list:
+            self.SyncCourse(
+                course=course, semester_id=semester_id,
+            )
+
+    def SyncCourse(self, course: types.CourseInfo, semester_id: str) -> bool:
+        file_list = self.get_file_list(
+            course_id=course.id, course_type=course.course_type
+        )
+        print(
+            f"Syncing Course {course.course_number} {course.name} {course.english_name} ......"
+        )
+        if self.sync_docs:
+            for file in file_list:
+                self.SyncFile(file, semester_id=semester_id, course=course)
+        if self.sync_work:
+            homework_list = self.get_homework_list(course_id=course.id)
+            for homework in homework_list:
+                self.SyncHomework(
+                    homework=homework, semester_id=semester_id, course=course
+                )
+
+    def SyncFile(
+        self, file: types.File, semester_id: str, course: types.CourseInfo
+    ) -> bool:
+        prefix = os.path.join(
+            self.prefix,
+            slugify(course.english_name),
+            slugify("documents"),
+            slugify(file.clazz),
+        )
+        filename = slugify(file.title) + slugify(
+            f".{file.file_type}" if file.file_type else ""
+        )
+        self.Download(url=file.download_url, prefix=prefix, filename=filename)
+        return True
+
+    def SyncHomework(
+        self, homework: types.Homework, semester_id: str, course: types.CourseInfo
+    ) -> bool:
+        prefix = os.path.join(
+            self.prefix,
+            slugify(course.english_name),
+            slugify("work"),
+            slugify(homework.title),
+        )
+        os.makedirs(prefix, exist_ok=True)
+        lines = []
+        lines.append(f"## Contents and Requirements")
+        lines.append(f"")
+        lines.append(f"### Title")
+        lines.append(f"")
+        lines.append(f"{homework.title}")
+        lines.append(f"")
+        lines.append(f"### Description")
+        lines.append(f"")
+        lines.append(f"{homework.description}")
+        lines.append(f"")
+        if homework.attachment:
+            filename = slugify(
+                f"{homework.title}{os.path.splitext(homework.attachment.name)[-1]}"
+            )
+            self.Download(
+                url=homework.attachment.download_url, prefix=prefix, filename=filename,
+            )
+            lines.append(f"### Attach.")
+            lines.append(f"")
+            lines.append(f"[{homework.attachment.name}]({filename})")
+            lines.append(f"")
+        lines.append(f"### ANS")
+        lines.append(f"")
+        lines.append(f"{homework.answer_content}")
+        lines.append(f"")
+        if homework.answer_attachment:
+            filename = slugify(
+                f"ans-{homework.title}{os.path.splitext(homework.answer_attachment.name)[-1]}"
+            )
+            self.Download(
+                url=homework.answer_attachment.download_url,
+                prefix=prefix,
+                filename=filename,
+            )
+            lines.append(f"### Attach.")
+            lines.append(f"")
+            lines.append(f"[{homework.answer_attachment.name}]({filename})")
+            lines.append(f"")
+        lines.append(f"### Deadline (GMT+8)")
+        lines.append(f"")
+        lines.append(f"{homework.deadline}")
+        lines.append(f"")
+        if self.sync_submit:
+            lines.append(f"## My coursework submitted")
+            lines.append(f"")
+            lines.append(f"### Content")
+            lines.append(f"")
+            lines.append(f"{homework.submitted_content}")
+            lines.append(f"")
+            if homework.submitted_attachment:
+                filename = slugify(
+                    f"submit-{homework.title}{os.path.splitext(homework.submitted_attachment.name)[-1]}"
+                )
+                self.Download(
+                    url=homework.submitted_attachment.download_url,
+                    prefix=prefix,
+                    filename=filename,
+                )
+                lines.append(f"### Attach.")
+                lines.append(f"")
+                lines.append(f"[{homework.submitted_attachment.name}]({filename})")
+                lines.append(f"")
+            lines.append(f"## Instructors' comments")
+            lines.append(f"")
+            lines.append(f"### By")
+            lines.append(f"")
+            lines.append(f"{homework.grader_name}")
+            lines.append(f"")
+            lines.append(f"### Date")
+            lines.append(f"")
+            lines.append(f"{homework.grade_time}")
+            lines.append(f"")
+            lines.append(f"### Grade")
+            lines.append(f"")
+            lines.append(f"{homework.grade}")
+            lines.append(f"")
+            lines.append(f"### Comment")
+            lines.append(f"")
+            lines.append(f"{homework.grade_content}")
+            lines.append(f"")
+            if homework.grade_attachment:
+                filename = slugify(
+                    f"comment-{homework.title}{os.path.splitext(homework.grade_attachment.name)[-1]}"
+                )
+                self.Download(
+                    url=homework.grade_attachment.download_url,
+                    prefix=prefix,
+                    filename=filename,
+                )
+                lines.append(f"### Attach.")
+                lines.append(f"")
+                lines.append(f"[{homework.grade_attachment.name}]({filename})")
+                lines.append(f"")
+        lines = [line + "\n" for line in lines]
+        filename = slugify("README.md")
+        with open(os.path.join(prefix, filename), "w") as file:
+            file.writelines(lines)
---
+++ thu_learn_lib/helper.py
@@ -0,0 +1,317 @@
+import urllib
+import bs4
+import datetime
+import re
+import requests
+
+
+from . import ty as types
+from . import urls
+
+
+class LearnHelper(requests.Session):
+    def __init__(self) -> None:
+        super().__init__()
+
+    @property
+    def token(self) -> str:
+        return self.cookies["XSRF-TOKEN"] if "XSRF-TOKEN" in self.cookies else None
+
+    def get_with_token(self, url: str, params: dict = None) -> requests.Response:
+        if params is None:
+            params = {"_csrf": self.token}
+        else:
+            params["_csrf"] = self.token
+        return self.get(url=url, params=params)
+
+    def login(self, username: str, password: str) -> bool:
+        response = self.get(urls.LEARN_PREFIX)
+        soup = bs4.BeautifulSoup(response.text, features="html.parser")
+        form = soup.find(
+            name="form", attrs={"class": "w", "id": "loginForm", "method": "post"}
+        )
+
+        payload = urls.id_login_form_data(username=username, password=password).params
+        response = self.post(url=form["action"], data=payload)
+        soup = bs4.BeautifulSoup(response.text, features="html.parser")
+        a = soup.find(name="a")
+        pattern = rf"{urls.LEARN_PREFIX}/f/login.do\?status=(\w*)&ticket=(\w*)"
+        match = re.match(pattern=pattern, string=a["href"])
+        self.success = match.group(1) == "SUCCESS"
+        self.ticket = match.group(2)
+
+        response = self.get(a["href"])
+        soup = bs4.BeautifulSoup(response.text, features="html.parser")
+        script = soup.find(name="script", type="text/javaScript")
+        pattern = r"\s*window.location=\"/b/j_spring_security_thauth_roaming_entry\?ticket=(\w*)\";\s*"
+        match = re.match(pattern=pattern, string=script.text)
+        self.ticket = match.group(1)
+
+        request = urls.learn_auth_roam(ticket=self.ticket)
+        response = self.get(url=request.url, params=request.params)
+
+        request = urls.learn_student_course_list_page()
+        response = self.get(url=request.url, params=request.params)
+
+        print(
+            f"User {username} login successfully"
+            if self.success
+            else f"User {username} login failed!"
+        )
+        return self.success
+
+    def logout(self) -> None:
+        request = urls.learn_logout()
+        response = self.post(url=request.url)
+
+    def get_semester_id_list(self) -> list:
+        request = urls.learn_semester_list()
+        response = self.get_with_token(url=request.url, params=request.params)
+        json = response.json()
+        return [x for x in json if x]
+
+    def get_current_semester(self) -> types.SemesterInfo:
+        request = urls.learn_current_semester()
+        response = self.get_with_token(url=request.url, params=request.params)
+        json = response.json()
+        result = json["result"]
+        return types.SemesterInfo(
+            id=result["id"],
+            start_date=datetime.datetime.strptime(result["kssj"], r"%Y-%m-%d").date(),
+            end_date=datetime.datetime.strptime(result["jssj"], r"%Y-%m-%d").date(),
+            start_year=int(result["xnxq"][0:4]),
+            end_year=int(result["xnxq"][5:9]),
+            type=int(result["xnxq"][10:]),
+        )
+
+    def get_course_list(
+        self, semester_id: str, course_type: types.CourseType = types.CourseType.STUDENT
+    ) -> list[types.CourseInfo]:
+        request = urls.learn_course_list(semester_id, course_type)
+        response = self.get_with_token(url=request.url, params=request.params)
+        json = response.json()
+        result = json["resultList"]
+
+        def mapper(course: dict):
+            request = urls.learn_course_time_location(course["wlkcid"])
+            response = self.get_with_token(request.url, request.params)
+            return types.CourseInfo(
+                id=course["wlkcid"],
+                name=course["kcm"],
+                english_name=course["ywkcm"],
+                time_and_location=response.json(),
+                url=str(urls.learn_course_url(course["wlkcid"], course_type)),
+                teacher_name=course["jsm"]
+                if course["jsm"]
+                else "",  # teacher can not fetch this
+                teacher_number=course["jsh"],
+                course_number=course["kch"],
+                course_index=int(
+                    course["kxh"]
+                ),  # course["kxh"] could be string (teacher mode) or number (student mode)
+                course_type=course_type,
+            )
+
+        courses = list(map(mapper, result))
+        return courses
+
+    def get_file_list(
+        self, course_id: str, course_type: types.CourseType = types.CourseType.STUDENT
+    ) -> list[types.File]:
+        request = urls.learn_file_classify(course_id=course_id)
+        response = self.get_with_token(url=request.url, params=request.params)
+        json = response.json()
+        records = json["object"]["rows"]
+        clazz = {}
+        for record in records:
+            clazz[record["kjflid"]] = record["bt"]  # 课件分类 ID, 标题
+
+        request = urls.learn_file_list(course_id=course_id, course_type=course_type)
+        response = self.get_with_token(request.url, request.params)
+        json = response.json()
+        result = []
+        if course_type == types.CourseType.STUDENT:
+            result = json["object"]
+        else:  # teacher
+            result = json["object"]["resultList"]
+
+        def mapper(file: dict) -> types.File:
+            title: str = file["bt"]
+            download_url: str = urls.learn_file_download(
+                file_id=file["wjid"]
+                if course_type == types.CourseType.STUDENT
+                else file["id"],
+                course_type=course_type,
+                course_id=course_id,
+            )
+            preview_url = None
+            return types.File(
+                id=file["wjid"],  # 文件 ID
+                title=file["bt"],  # 标题
+                description=file["ms"],  # 描述
+                raw_size=file["wjdx"],  # 文件大小
+                size=file["fileSize"],
+                upload_time=datetime.datetime.strptime(
+                    file["scsj"], r"%Y-%m-%d %H:%M"
+                ),  # 上传时间
+                download_url=str(download_url),
+                preview_url=str(preview_url),
+                is_new=file["isNew"],
+                marked_important=file["sfqd"] == 1,  # 是否强调
+                visit_count=file["llcs"] or 0,  # 浏览次数
+                download_count=file["xzcs"] or 0,  # 下载次数
+                file_type=file["wjlx"],  # 文件类型
+                remote_file=types.RemoteFile(
+                    id=file["wjid"],  # 文件 ID
+                    name=title,
+                    download_url=str(download_url),
+                    preview_url=str(preview_url),
+                    size=file["fileSize"],
+                ),
+                clazz=clazz[file["kjflid"]],  # 课件分类 ID
+            )
+
+        return list(map(mapper, result))
+
+    def get_homework_list(
+        self, course_id: str, course_type: types.CourseType = types.CourseType.STUDENT
+    ) -> list[types.Homework]:
+        result = []
+        request = urls.learn_homework_list_new(course_id=course_id)
+        result += self.get_homework_list_at_url(
+            request=request, status=types.HomeworkStatus(submitted=False, graded=False)
+        )
+        request = urls.learn_homework_list_submitted(course_id=course_id)
+        result += self.get_homework_list_at_url(
+            request=request, status=types.HomeworkStatus(submitted=True, graded=False)
+        )
+        request = urls.learn_homework_list_graded(course_id=course_id)
+        result += self.get_homework_list_at_url(
+            request=request, status=types.HomeworkStatus(submitted=True, graded=True)
+        )
+
+        return result
+
+    def get_homework_list_at_url(
+        self, request: urls.URL, status: types.HomeworkStatus
+    ) -> list[types.Homework]:
+        response = self.get_with_token(url=request.url, params=request.params)
+        json = response.json()
+
+        result = json["object"]["aaData"]
+
+        def mapper(work: dict) -> types.Homework:
+            detail = self.parse_homework_detail(
+                course_id=work["wlkcid"],  # 课程 ID
+                homework_id=work["zyid"],  # 作业 ID
+                student_homework_id=work["xszyid"],  # 学生作业 ID
+            )
+
+            return types.Homework(
+                id=work["zyid"],  # 作业 ID
+                student_homework_id=work["xszyid"],  # 学生作业 ID
+                title=work["bt"],  # 标题
+                url=str(
+                    urls.learn_homework_detail(
+                        course_id=work["wlkcid"],  # 课程 ID
+                        homework_id=work["zyid"],  # 作业 ID
+                        student_homework_id=work["xszyid"],  # 学生作业 ID
+                    )
+                ),
+                deadline=work["jzsj"],  # 截止时间
+                submit_url=urls.learn_homework_submit(
+                    work["wlkcid"], work["xszyid"]  # 课程 ID, 学生作业 ID
+                ),
+                submit_time=work["scsj"],  # 上传时间
+                grade=work["cj"],  # 成绩
+                grader_name=work["jsm"],  # 教师名
+                grade_content=work["pynr"],  # 批阅内容
+                grade_time=work["pysj"],  # 批阅时间
+                submitted=status.submitted,
+                graded=status.graded,
+                description=detail.description,
+                answer_content=detail.answer_content,
+                submitted_content=detail.submitted_content,
+                attachment=detail.attachment,
+                answer_attachment=detail.answer_attachment,
+                submitted_attachment=detail.submitted_attachment,
+                grade_attachment=detail.grade_attachment,
+            )
+
+        return list(map(mapper, result))
+
+    def parse_homework_detail(
+        self, course_id: str, homework_id: str, student_homework_id: str
+    ) -> types.HomeworkDetail:
+        request = urls.learn_homework_detail(
+            course_id=course_id,
+            homework_id=homework_id,
+            student_homework_id=student_homework_id,
+        )
+        response = self.get_with_token(request.url, request.params)
+        text = response.text
+        soup = bs4.BeautifulSoup(markup=text, features="html.parser")
+
+        div_list_calendar_clearfix = soup.find_all(
+            name="div", attrs={"class": "list calendar clearfix"}
+        )
+        div_fl_right = sum(
+            [
+                div.find_all(name="div", attrs={"class": "fl right"})
+                for div in div_list_calendar_clearfix
+            ],
+            [],
+        )
+        div_c55 = sum(
+            [div.find_all(name="div", attrs={"class": "c55"}) for div in div_fl_right],
+            [],
+        )
+        description = div_c55[0].getText()
+        answer_content = div_c55[1].getText()
+        div_box = soup.find_all(name="div", attrs={"class": "boxbox"})
+        div_box = div_box[1]
+        div_right = div_box.find_all(name="div", attrs={"class": "right"})
+        submitted_content = div_right = div_right[2].getText()
+
+        div_list_fujian_clearfix = soup.find_all(
+            name="div", attrs={"class": "list fujian clearfix"}
+        )
+        return types.HomeworkDetail(
+            description=description.strip(),
+            answer_content=answer_content.strip(),
+            submitted_content=submitted_content.strip(),
+            attachment=self.parse_homework_file(div_list_fujian_clearfix[0])
+            if len(div_list_fujian_clearfix) > 0
+            else None,
+            answer_attachment=self.parse_homework_file(div_list_fujian_clearfix[1])
+            if len(div_list_fujian_clearfix) > 1
+            else None,
+            submitted_attachment=self.parse_homework_file(div_list_fujian_clearfix[2])
+            if len(div_list_fujian_clearfix) > 2
+            else None,
+            grade_attachment=self.parse_homework_file(div_list_fujian_clearfix[3])
+            if len(div_list_fujian_clearfix) > 3
+            else None,
+        )
+
+    def parse_homework_file(self, div) -> types.RemoteFile:
+        ftitle = div.find(name="span", attrs={"class": "ftitle"}) or div.find(
+            name="span", attrs={"class", "ft"}
+        )
+        if not ftitle:
+            return None
+        a = ftitle.find(name="a")
+        size = div.find(name="span", attrs={"class": "color_999"})
+        size = size.getText()
+        params = dict(urllib.parse.parse_qsl(urllib.parse.urlsplit(a["href"]).query))
+        attachment_id = params["fileId"]
+        download_url = urls.LEARN_PREFIX + (
+            params["downloadUrl"] if "downloadUrl" in params else a["href"]
+        )
+        return types.RemoteFile(
+            id=attachment_id,
+            name=a.getText(),
+            download_url=download_url,
+            preview_url=None,
+            size=size.strip(),
+        )
